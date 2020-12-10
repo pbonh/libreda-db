323f98586dd656f3e77b5fce944182c4a2a08db8
Thomas Kramer <code@tkramer.ch>
2020-12-10 19:58:04 +0100

    NetlistReader `read_into_netlist.
    * `read_into_netlist` is the new name for reading a netlist and updating
    a mutable netlist.
    * `read_netlist` creates a fresh netlist and populates it from the
    input.

--- src/netlist/io.rs
+++ src/netlist/io.rs
@@ -25,13 +25,23 @@ use super::netlist::Netlist;
 pub trait NetlistReader {
     /// Type of error that could happen while reading a netlist.
     type Error;
+
     /// Read a netlist from a byte stream and populate the netlist data structure.
-    fn read_netlist<R: Read>(&self, reader: &mut R, netlist: &mut Netlist) -> Result<(), Self::Error>;
+    fn read_into_netlist<R: Read>(&self, reader: &mut R, netlist: &mut Netlist) -> Result<(), Self::Error>;
+
+    /// Read a netlist from a byte stream.
+    fn read_netlist<R: Read>(&self, reader: &mut R) -> Result<Netlist, Self::Error> {
+        let mut netlist = Netlist::new();
+        self.read_into_netlist(reader, &mut netlist)?;
+        Ok(netlist)
+    }
+
 }
 
 pub trait NetlistWriter {
     /// Type of error that could happen while writing a netlist.
     type Error;
+
     /// Write the netlist data structure to a byte stream.
     fn write_netlist<W: Write>(&self, writer: &mut W, netlist: &Netlist) -> Result<(), Self::Error>;
 }

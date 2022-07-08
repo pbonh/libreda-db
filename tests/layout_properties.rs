f1c4b1c54e1a7d7805336bbeb5010c0612979367
Axel Viala <axel.viala@darnuria.eu>
2022-07-08 18:26:12 +0200

    Cargo clippy --fix

--- tests/layout_properties.rs
+++ tests/layout_properties.rs
@@ -11,7 +11,7 @@ fn test_shape_properties() {
     let top_cell = layout.create_cell("TOP".into());
     let rect = Rect::new((0, 0), (10, 10));
     let rect_id = layout.insert_shape(&top_cell, &layer, rect.into());
-    let key: <Chip as HierarchyBase>::NameType = "name".to_string().into();
+    let key: <Chip as HierarchyBase>::NameType = "name".to_string();
     layout.set_shape_property(&rect_id, key.clone(), "Rectangle".into());
 
     assert_eq!(layout.get_shape_property(&rect_id, &key)

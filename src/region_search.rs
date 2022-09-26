f923bac255f5fd0e0616306f32c56488d5c188d6
Thomas Kramer <code@tkramer.ch>
2022-09-26 22:01:11 +0200

    construct initial RTrees

--- src/region_search.rs
+++ src/region_search.rs
@@ -2,93 +2,243 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-//! Acquire performance metrics of single data-base functions.
+//! Add fast region queries to layouts.
+
+use fnv::FnvHashMap;
+use num_traits::{PrimInt, Signed};
+use rstar::{RTree, RTreeObject};
 
 use crate::prelude::*;
 use crate::decorator::{Decorator, MutDecorator};
 use crate::decorator::hierarchy::*;
 use crate::decorator::layout::*;
 use crate::decorator::netlist::*;
-
-use std::time::Duration;
-
+use crate::decorator::l2n::*;
 
 
-/// Wrapper around netlist, layout and L2N structures that allows measuring time spend in function calls.
+/// Wrapper around netlist, layout and L2N structures that allows fast region queries.
 ///
 /// # Types
 /// * `T`: Underlying data structure.
-pub struct DBPerf<'a, T> {
+pub struct RegionSearch<'a, T>
+    where T: LayoutBase,
+          T::Coord: PrimInt + Signed + std::fmt::Debug {
     /// Underlying data structure.
     chip: &'a mut T,
-    perf_data: PerfCounters,
+    /// RTrees containing geometric shapes.
+    shape_rtrees: FnvHashMap<T::CellId,
+        FnvHashMap<
+            T::LayerId,
+            RTree<ShapeEntry<T::ShapeId, T::Coord>>
+        >
+    >,
+    /// RTree containing child instances.
+    instance_rtree: FnvHashMap<T::CellId, RTree<CellInstanceEntry<T>>>,
+    /// Cache for bounding boxes of cells.
+    cell_bounding_boxes: FnvHashMap<T::CellId, Rect<T::Coord>>,
 }
 
-impl<'a, T> DBPerf<'a, T> {
+/// Wrapper for shapes.
+#[derive(Debug, Clone, PartialEq)]
+pub struct ShapeEntry<ShapeId, Coord> {
+    bounding_box: Rect<Coord>,
+    shape_id: ShapeId,
+}
 
-    /// Wrap the `chip` structure into a performance counter.
+/// Wrapper for cell instances
+#[derive(Debug, Clone, PartialEq)]
+pub struct CellInstanceEntry<L: LayoutBase> {
+    bounding_box: Rect<L::Coord>,
+    cell_inst_id: L::CellInstId,
+}
+
+// Make `ShapeEntry` usable within RTrees.
+impl<ShapeId, Coord> BoundingBox<Coord> for ShapeEntry<ShapeId, Coord>
+    where Coord: PrimInt {
+    fn bounding_box(&self) -> Rect<Coord> {
+        self.bounding_box
+    }
+}
+
+// Make `ShapeEntry` usable within RTrees.
+impl<ShapeId, Coord> RTreeObject for ShapeEntry<ShapeId, Coord>
+    where Coord: PrimInt + Signed + std::fmt::Debug {
+    type Envelope = rstar::AABB<[Coord; 2]>;
+
+    fn envelope(&self) -> Self::Envelope {
+        let bbox = self.bounding_box();
+
+        rstar::AABB::from_corners(
+            bbox.lower_left().into(),
+            bbox.upper_right().into(),
+        )
+    }
+}
+
+// Make `CellInstanceEntry` usable within RTrees.
+impl<L> BoundingBox<L::Coord> for CellInstanceEntry<L>
+    where L: LayoutBase,
+          L::Coord: PrimInt {
+    fn bounding_box(&self) -> Rect<L::Coord> {
+        self.bounding_box
+    }
+}
+
+// Make `CellInstanceEntry` usable within RTrees.
+impl<L> RTreeObject for CellInstanceEntry<L>
+    where L: LayoutBase,
+          L::Coord: PrimInt + Signed + std::fmt::Debug {
+    type Envelope = rstar::AABB<[L::Coord; 2]>;
+
+    fn envelope(&self) -> Self::Envelope {
+        let bbox = self.bounding_box();
+
+        rstar::AABB::from_corners(
+            bbox.lower_left().into(),
+            bbox.upper_right().into(),
+        )
+    }
+}
+
+impl<'a, T> RegionSearch<'a, T>
+    where T: LayoutBase,
+          T::Coord: PrimInt + Signed + std::fmt::Debug {
+    /// Add fast region query capability to `chip`.
     pub fn new(chip: &'a mut T) -> Self {
-        Self {
+        let mut region_search = Self {
             chip,
-            perf_data: Default::default()
+            shape_rtrees: Default::default(),
+            instance_rtree: Default::default(),
+            cell_bounding_boxes: Default::default(),
+        };
+
+        region_search.create_shape_trees();
+        region_search.create_cell_instance_trees();
+
+        region_search
+    }
+
+    fn create_shape_trees(&mut self) {
+        // Process cells starting with leaf-cells to top cells.
+        let mut cell_rtrees: FnvHashMap<_, _> = Default::default();
+        for cell in self.chip.each_cell_bottom_to_top() {
+            let mut rtrees: FnvHashMap<_, RTree<_>> = Default::default();
+
+            for layer in self.chip.each_layer() {
+                // Collect all shapes which have a bounding box.
+                let mut all_shapes = vec![];
+                self.chip.for_each_shape(&cell, &layer, |shape_id, geometry| {
+                    if let Some(bounding_box) = geometry.try_bounding_box() {
+                        let rtree_entry = ShapeEntry {
+                            bounding_box,
+                            shape_id: shape_id.clone(),
+                        };
+                        all_shapes.push(rtree_entry)
+                    }
+                });
+
+                // Create RTree.
+                let rtree = RTree::bulk_load(all_shapes);
+
+                rtrees.insert(layer, rtree);
+            }
+            cell_rtrees.insert(cell, rtrees);
+        }
+
+        self.shape_rtrees = cell_rtrees;
+    }
+
+    fn create_cell_instance_trees(&mut self) {
+        for cell in self.chip.each_cell_bottom_to_top() {
+            // Compute bounding box of the shapes (without subcells).
+            let shape_bbox = self.compute_shape_bbox(&cell);
+
+            // Register all instances in the rtree (their bounding boxes are known by now).
+            let instance_entries: Vec<_> = self.chip.each_cell_instance(&cell)
+                .filter_map(|inst| {
+                    self.cell_bounding_boxes.get(&self.chip.template_cell(&inst))
+                        .map(|bbox| (inst, bbox))
+                })
+                .map(|(inst, bbox)| {
+                    let tf = self.chip.get_transform(&inst);
+                    let bbox_transformed = bbox.transform(|p| tf.transform_point(p));
+
+                    CellInstanceEntry {
+                        bounding_box: bbox_transformed,
+                        cell_inst_id: inst,
+                    }
+                })
+                .collect();
+
+            let rtree = RTree::bulk_load(instance_entries);
+
+            let cell_inst_bbox = if rtree.size() > 0 {
+                let envelope = rtree.root().envelope();
+                let bbox = Rect::new(envelope.lower(), envelope.upper());
+                Some(bbox)
+            } else {
+                None
+            };
+
+            // Combine both bounding boxes.
+            let bbox = cell_inst_bbox.into_iter()
+                .chain(shape_bbox)
+                .reduce(|acc, b| acc.add_rect(&b));
+
+            if let Some(bbox) = bbox {
+                self.cell_bounding_boxes.insert(cell.clone(), bbox);
+            }
+
+            self.instance_rtree.insert(cell, rtree);
         }
     }
 
-    /// Access the performance counters.
-    pub fn perf_data(&self) -> &PerfCounters {
-        &self.perf_data
+    /// Compute the bounding box of all shapes in a cell, excluding sub cells.
+    fn compute_shape_bbox(&self, cell_id: &T::CellId) -> Option<Rect<T::Coord>> {
+        self.shape_rtrees.get(cell_id).into_iter()
+            // Get rtree of each layer.
+            .flat_map(|layer_trees| layer_trees.values())
+            // Get the bounding box of the layer.
+            .map(|rtree| rtree.root().envelope())
+            // Convert to Rect
+            .map(|envelope| Rect::new(envelope.lower(), envelope.upper()))
+            // Reduce to a single bounding box.
+            .reduce(|acc, b| acc.add_rect(&b))
     }
-}
 
-/// Statistics on function calls of the DB.
-#[derive(Debug, Default, Clone)]
-pub struct PerfCounters {
-    /// Performance counters of `insert_shape()`.
-    pub insert_shape: PerfCounter,
-}
-
-/// Statistics on calls of a single function.
-#[derive(Debug, Default, Clone)]
-pub struct PerfCounter {
-    /// Number of function calls.
-    pub num_calls: usize,
-    /// Total time spent in this function.
-    pub total_time: Duration,
-}
-
-/// Context manager for a performance counter.
-/// This is used to track when the measured function exits.
-pub struct PerfCounterManager<'a> {
-    /// Instant then the time measurement was started.
-    start_time: std::time::Instant,
-    counter: &'a mut PerfCounter,
-}
-
-impl<'a> PerfCounterManager<'a> {
-    /// Stop measuring the time and add the duration to the total spent time.
-    fn stop_measurement(self) {
-            let elapsed = self.start_time.elapsed();
-            self.counter.total_time += elapsed;
-
-    }
-}
-
-impl PerfCounter {
-    /// Start measuring the spent time.
-    /// Must call `stop()` on the return value.
-    #[must_use]
-    fn start_measurement(&mut self) -> PerfCounterManager {
-        self.num_calls += 1;
-        PerfCounterManager {
-            start_time: std::time::Instant::now(),
-            counter: self
+    /// Compute bounding box of all subcells, excluding shapes in the current cell.
+    fn compute_subcell_bboxes(&self, cell_id: &T::CellId) -> Option<Rect<T::Coord>> {
+        if let Some(instance_rtree) = self.instance_rtree.get(cell_id) {
+            if instance_rtree.size() > 0 {
+                let envelope = instance_rtree.root().envelope();
+                Some(Rect::new(envelope.lower(), envelope.upper()))
+            } else {
+                None
+            }
+        } else {
+            None
         }
+        // let subcell_bboxes = self.chip.each_cell_instance(cell_id)
+        //     .filter_map(|inst| {
+        //         self.cell_bounding_boxes.get(&self.chip.template_cell(&inst))
+        //             .map(|bbox| (inst, bbox))
+        //     })
+        //     .map(|(inst, bbox)| {
+        //         let tf = self.chip.get_transform(&inst);
+        //         let bbox_transformed = bbox.transform(|p| tf.transform_point(p));
+        //         bbox_transformed
+        //     });
+        //
+        // subcell_bboxes.reduce(|acc, b| {
+        //     acc.add_rect(&b)
+        // })
     }
-
-
 }
 
-impl<'a, T> Decorator for DBPerf<'a, T> {
+
+impl<'a, T> Decorator for RegionSearch<'a, T>
+    where T: LayoutBase,
+          T::Coord: PrimInt + Signed + std::fmt::Debug {
     type D = T;
 
     fn base(&self) -> &Self::D {
@@ -96,7 +246,9 @@ impl<'a, T> Decorator for DBPerf<'a, T> {
     }
 }
 
-impl<'a, T> MutDecorator for DBPerf<'a, T> {
+impl<'a, T> MutDecorator for RegionSearch<'a, T>
+    where T: LayoutBase,
+          T::Coord: PrimInt + Signed + std::fmt::Debug {
     fn mut_base(&mut self) -> &mut Self::D {
         &mut self.chip
     }
@@ -104,41 +256,91 @@ impl<'a, T> MutDecorator for DBPerf<'a, T> {
 
 
 // Inherit everything from HierarchyBase.
-impl<'a, H: HierarchyBase + 'static> HierarchyBaseDecorator for DBPerf<'a, H> {
+impl<'a, H> HierarchyBaseDecorator for RegionSearch<'a, H>
+    where H: LayoutBase + 'static,
+          H::Coord: PrimInt + Signed + std::fmt::Debug {
     type NameType = H::NameType;
     type CellId = H::CellId;
     type CellInstId = H::CellInstId;
 }
 
-// Inherit everything from LayoutBase.
-impl<'a, L: LayoutBase + 'static> LayoutBaseDecorator for DBPerf<'a, L> {}
+//
+// // Inherit everything from LayoutBase.
+impl<'a, L> LayoutBaseDecorator for RegionSearch<'a, L>
+    where L: LayoutBase + 'static,
+          L::Coord: PrimInt + Signed + std::fmt::Debug
+{}
 
 
 // Inherit everything from NetlistBase.
-impl<'a, N: NetlistBase + 'static> NetlistBaseDecorator for DBPerf<'a, N> {}
+impl<'a, N> NetlistBaseDecorator for RegionSearch<'a, N>
+    where N: LayoutBase + NetlistBase + 'static,
+          N::Coord: PrimInt + Signed + std::fmt::Debug {}
+
+impl<'a, LN> L2NBaseDecorator for RegionSearch<'a, LN>
+    where LN: L2NBase + 'static,
+          LN::Coord: PrimInt + Signed + std::fmt::Debug
+{}
 
 // Inherit everything from HierarchyEdit.
-impl<'a, H: HierarchyEdit + 'static> HierarchyEditDecorator for DBPerf<'a, H> {
-
+impl<'a, H> HierarchyEditDecorator for RegionSearch<'a, H>
+    where H: HierarchyEdit + LayoutBase + 'static,
+          H::Coord: PrimInt + Signed + std::fmt::Debug
+{
     fn d_new() -> Self {
         unimplemented!()
     }
-
 }
 
 // Inherit everything from LayoutEdit.
-// TODO: Since every function should be measured, implement LayoutEdit directly without using the '*Decorator' traits.
-impl<'a, L: LayoutEdit + 'static> LayoutEditDecorator for DBPerf<'a, L> {
+impl<'a, L> LayoutEditDecorator for RegionSearch<'a, L>
+    where L: LayoutEdit + 'static,
+          L::Coord: PrimInt + Signed + std::fmt::Debug {
 
     fn d_insert_shape(&mut self, parent_cell: &L::CellId, layer: &L::LayerId, geometry: Geometry<L::Coord>) -> L::ShapeId {
-        let m = self.perf_data.insert_shape.start_measurement();
-        let ret = self.chip.insert_shape(parent_cell, layer, geometry);
-        m.stop_measurement();
-        ret
+        todo!("update RTree");
+        let shape_id = self.chip.insert_shape(parent_cell, layer, geometry);
+        shape_id
     }
 
+    fn d_remove_shape(&mut self, shape_id: &L::ShapeId) -> Option<Geometry<L::Coord>> {
+        let _geo = self.chip.remove_shape(shape_id);
+        todo!("update RTree")
+    }
+
+    fn d_replace_shape(&mut self, shape_id: &L::ShapeId, geometry: Geometry<L::Coord>) -> Geometry<L::Coord> {
+        let _geo = self.chip.replace_shape(shape_id, geometry);
+        todo!("update RTree")
+    }
+
+    fn d_set_transform(&mut self, cell_inst: &L::CellInstId, tf: SimpleTransform<L::Coord>) {
+        self.chip.set_transform(cell_inst, tf);
+        todo!("update RTree")
+    }
 }
 
 
 // Inherit everything from NetlistBase.
-impl<'a, N: NetlistEdit + 'static> NetlistEditDecorator for DBPerf<'a, N> {}
+impl<'a, N: NetlistEdit + 'static> NetlistEditDecorator for RegionSearch<'a, N>
+    where N: LayoutBase + NetlistEdit + 'static,
+          N::Coord: PrimInt + Signed + std::fmt::Debug {}
+
+
+impl<'a, LN> L2NEditDecorator for RegionSearch<'a, LN>
+    where LN: L2NEdit + 'static,
+          LN::Coord: PrimInt + Signed + std::fmt::Debug {}
+
+
+#[test]
+fn test_create_region_search() {
+
+    let mut chip = Chip::new();
+
+    let top = chip.create_cell("TOP".to_string().into());
+    let layer1 = chip.create_layer(1, 0);
+    chip.insert_shape(&top, &layer1, Rect::new((0, 0), (10, 10)).into());
+
+    let region_search = RegionSearch::new(&mut chip);
+
+    assert_eq!(region_search.cell_bounding_boxes[&top], Rect::new((0, 0), (10, 10)));
+}
